d84f781c6a9bb81989e02c0b13da1dcf789807ec
T1b4lt <guillermosegfer@gmail.com>
2019-11-14 12:02:08 +0100

    change name and first aproach to solution

--- src/ExcelComparator.py
+++ src/ExcelComparator.py
@@ -1,24 +1,43 @@
 import argparse
 
+import pandas as pd
 
-def main(args):
-	print("hola")
+
+def main(cliargs):
+	file1_name = cliargs.first
+	file2_name = cliargs.second
+
+	file1_path = "../input/" + file1_name
+	file2_path = "../input/" + file2_name
+
+	print("Comparando el fichero", file1_path, "con el fichero", file2_path)
+
+	file1 = pd.read_csv(file1_path, delimiter=";")
+	file2 = pd.read_csv(file2_path, delimiter=";")
+
+	print(file1.head())
+	print(file2.head())
+
+	for index, value in file1.iterrows():
+		row = ""
+		for column in value:
+			row += str(column) + " "
+		print(row)
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
-		description="Excel comparator",
-		epilog="An excel comparator. It checks if both excels have the same records.")
+		description="csv comparator",
+		epilog="An csv comparator. It checks if both csv have the same records.")
 
-	"""
 	parser.add_argument(
 		"first",
-		help="first excel",
-		metavar="FST")
+		help="first csv",
+		metavar="first-file")
 	parser.add_argument(
 		"second",
-		help="second excel",
-		metavar="SEC")
-	"""
+		help="second csv",
+		metavar="second-file")
+
 	args = parser.parse_args()
 	main(args)
